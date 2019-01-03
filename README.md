ec526a79b61129542c189eb23c9579ace8574702
Steve Tingiris <steve@dabblelab.com>
2019-01-03 14:34:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Entity Resolution Starter Template
+---
+
---
+++ lambda/custom/index.js
@@ -0,0 +1,117 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk-core');
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechText = 'Hello there. How would you say we\'re doing as a company?';
+    const repromptText = 'Can you tell me how you think we\'re doing?';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(repromptText)
+      .withSimpleCard('Example Card Title', "Example card body content.")
+      .getResponse();
+  },
+};
+
+const RatingIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'RatingIntent';
+  },
+  handle(handlerInput) {
+
+    let speechText = `I'm not sure what that means.`;
+    const ratingSlot = handlerInput.requestEnvelope.request.intent.slots.rating.value;
+
+    if (ratingSlot) { 
+
+      let resolution = handlerInput.requestEnvelope.request.intent.slots.rating.resolutions.resolutionsPerAuthority[0];
+
+      if (resolution.status.code === "ER_SUCCESS_MATCH") {
+        let id = resolution.values[0].value.id;
+        speechText = `I heard you say we're doing ${ratingSlot}. On a scale of one to three, that's a ${id}.`;
+      } 
+
+    } else {
+      speechText = `I'm not sure what that means.`
+    }
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .getResponse();
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'You can rate us by saying how you think we\'re doing as a company. Say something like: I think you\'re doing great.';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Goodbye!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak(`Sorry, an error occurred. The error message was ${error.message}. Please say something else or repeat to recreate the error again.`)
+      .reprompt('Please say something else or repeat to recreate the error again.')
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    RatingIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
